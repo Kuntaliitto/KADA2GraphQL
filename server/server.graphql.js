f4eebbbe55f43789fb44277db818560da19015ce
Antti Tuppurainen <antti.tuppurainen@industry62.com>
2019-11-12 12:48:10 +0200

    Find TermByID, body:value as bodyValue cleaning

--- server/server.graphql.js
+++ server/server.graphql.js
@@ -1,4 +1,5 @@
 const { ElasticSearchClient } = require('./server.elasticsearch');
+const { ElasticSearchClientTerms } = require('./server.elasticsearch');
 const elasticSearchSchema = require('./server.es.schema');
 const { makeExecutableSchema } = require('graphql-tools');
 
@@ -9,6 +10,7 @@ const typeDefs = `
         page(nid: Int!): [Pages]
         news(nid: Int!): [NewsItems]
         news_items: [NewsItems]
+        term(tid: Int!): [Keywords]
     }
 
     type Pages {
@@ -18,6 +20,8 @@ const typeDefs = `
         type: String
         uuid: String
         changed: String
+        bodySummary: String    
+        bodyValue: String
         search_api_language: String
         pori_rendered_entity: String
     }
@@ -29,16 +33,51 @@ const typeDefs = `
         type: String
         uuid: String
         changed: String
+        bodySummary: String    
+        bodyValue: String
         search_api_language: String
         pori_rendered_entity: String
+    }
+
+    type Keywords {
+        id: Int
+        name: String
+        description: String
+        uuid: String
+        vocabulary: Int
+        node_count: String
+        url: String
+        search_api_viewed: String
     }
 `;
 
 // The root provides a resolver function for each API endpoint
 const resolvers = {
     Query: {
+        term: (_, { tid }) => new Promise((resolve, reject) => {
+            ElasticSearchClientTerms({
+                "query": {
+                    "multi_match": {
+                        "query": tid,
+                        "fields": ["tid", "id"]
+                    }
+                } })
+                .then(r => {
+                    let _source = r['hits']['hits'];
+                    _source.map((item, i) => _source[i] = item._source);
+
+                    resolve(_source);
+                });
+        }),
+
         pages: () => new Promise((resolve, reject) => {
-            ElasticSearchClient({ ...elasticSearchSchema })
+            ElasticSearchClient({
+                "query": {
+                    "multi_match": {
+                        "query": "page",
+                        "fields": ["type"]
+                    }
+                } })
                 .then(r => {
                     let _source = r['hits']['hits'];
                     _source.map((item, i) => _source[i] = item._source);
@@ -57,7 +96,17 @@ const resolvers = {
             })
                 .then(r => {
                     let _source = r['hits']['hits'];
+                    //Clean some drupal fields
+                    _source.forEach(item => {
+                        //console.log(item);
+                        item._source['bodyValue'] = item._source['body:value'];
+                        item._source['bodySummary'] = item._source['body:summary'];
+                        delete item._source['body:value'];
+                        delete item._source['body:summary'];
+                    })
+                    
                     _source.map((item, i) => _source[i] = item._source);
+                    
                     resolve(_source);
                 });
         }),
@@ -88,6 +137,14 @@ const resolvers = {
             })
                 .then(r => {
                     let _source = r['hits']['hits'];
+                    //Clean some drupal fields
+                    _source.forEach(item => {
+                        //console.log(item);
+                        item._source['bodyValue'] = item._source['body:value'];
+                        item._source['bodySummary'] = item._source['body:summary'];
+                        delete item._source['body:value'];
+                        delete item._source['body:summary'];
+                    })
                     _source.map((item, i) => _source[i] = item._source);
 
                     resolve(_source);
